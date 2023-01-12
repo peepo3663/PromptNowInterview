3328d6c763489ca30243c46713810d36c600be2c
wasupolbeans <wasupol@beans.ai>
2023-01-11 19:04:34 -0800

    Initial Commit

---
+++ Prompt Now Interview/Prompt_Now_InterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prompt_Now_InterviewApp.swift
+//  Prompt Now Interview
+//
+//  Created by Peter Beans on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Prompt_Now_InterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
