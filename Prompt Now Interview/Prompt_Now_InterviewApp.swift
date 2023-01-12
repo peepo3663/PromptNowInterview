fd1cc7877b410541caf6d449c0f18b7c09c751b6
wasupolbeans <wasupol@beans.ai>
2023-01-11 20:20:37 -0800

    [LowRisk] Complete Quiz 1 No 3

--- Prompt Now Interview/Prompt_Now_InterviewApp.swift
+++ Prompt Now Interview/Prompt_Now_InterviewApp.swift
@@ -71,7 +71,28 @@ struct Prompt_Now_InterviewApp: App {
         }
     }
     
-    private func printOutQuiz1No3() {
-        
+    private func printOutQuiz1No3(_ heightOfPyramid: Int =  5) {
+        print("Programming and Algorithm 3")
+        let xChar = "X"
+        let fillChar = "0"
+        for i in 1...heightOfPyramid {
+            var numberOfX = 0
+            let numberOfSpace = heightOfPyramid - i
+            if numberOfSpace > 0 {
+                for _ in 1...numberOfSpace {
+                    print(fillChar, terminator: "")
+                }
+            }
+            while (numberOfX != 2 * i - 1) {
+                print(xChar, terminator: "")
+                numberOfX += 1
+            }
+            if numberOfSpace > 0 {
+                for _ in 1...numberOfSpace {
+                    print(fillChar, terminator: "")
+                }
+            }
+            print("")
+        }
     }
 }
